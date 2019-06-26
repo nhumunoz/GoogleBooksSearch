4eafa07fd3fab6a11028e4b9236b5a54f9523eb1
nhurichie <richnhu@gmail.com>
2019-06-25 22:15:30 -0500

    Small stuff

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
