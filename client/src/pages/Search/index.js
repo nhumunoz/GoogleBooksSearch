6d12e0a9064d241e1640c0c4f5d724473ecc5db8
nhurichie <richnhu@gmail.com>
2019-06-25 22:01:08 -0500

    Styling and API Connection

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -17,16 +17,20 @@ export default class Search extends Component {
   handleSearch = () => {
     googleApi.search(this.state.queryString)
       .then(response => {
-        let books = Promise.all(response.data.items.map(book => {
-          return {
-            googleId: book.id,
+        let books = response.data.items.map(book => {
+          if (!book.volumeInfo.imageLinks){
+            book.volumeInfo.imageLinks = {thumbnail: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiL_OCykYbjAhUXac0KHX27D9UQjRx6BAgBEAU&url=https%3A%2F%2Fwww.maxpixel.net%2FBook-Cover-Empty-Book-Isolated-3057901&psig=AOvVaw1MQT4rWO9-X4v8jApgH83D&ust=1561603727572039"}
+          }
+          let result =  {
             title: book.volumeInfo.title,
             authors: book.volumeInfo.authors,
             description: book.volumeInfo.description,
             imageUrl: book.volumeInfo.imageLinks.thumbnail,
             link: book.volumeInfo.canonicalVolumeLink
           }
-        }));
+          console.log(result);
+          return result;
+        });
         this.setState({ books: books });
       });
   }
@@ -41,8 +45,20 @@ export default class Search extends Component {
           <FormBtn onClick={this.handleSearch}>Search Book</FormBtn>
 
           {(this.state.books.length > 0) ?
-            this.state.books.map(book => {
-              return (<div>{book.title}</div>);
+            this.state.books.map((book,i )=> {
+              return (
+              <div key={i} className="book">
+                <img src={book.imageUrl}/>
+                <div>
+                  <div>
+                    {book.title}
+                  </div>
+                  <p>
+                    {book.description}
+                  </p>
+                  <a href= {book.link}>Go to book!</a>
+                </div>
+              </div>);
             }) : "Search for a book!"}
 
         </div>
